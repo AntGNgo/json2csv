237edf18eba3862c7cd2ba8068b0f17a69152fdc
Anthony Ngo <ango.ext@yieldengineering.net>
2023-08-03 09:41:35 -0700

    check for array or single object

--- app.js
+++ app.js
@@ -2,10 +2,42 @@ const jsonField = document.getElementById("input");
 const submitBtn = document.getElementById("submit");
 const convertedValue = document.getElementById("converted-value");
 
+const convertJSON = (input) => {
+  try {
+    const jsonKeys = Object.keys(input);
+    const jsonValues = Object.values(input);
+
+    let index = 0;
+    jsonKeys.forEach((key) => {
+      index++;
+      if (index === jsonKeys.length) {
+        csv += key + `\n`;
+      } else {
+        csv += key + ", ";
+      }
+    });
+
+    index = 0;
+    jsonValues.forEach((value) => {
+      index++;
+      if (index === jsonValues.length) {
+        csv += value + `\n`;
+      } else {
+        csv += value + ", ";
+      }
+    });
+    console.log(csv);
+  } catch (error) {
+    console.log(error);
+    console.log("Error. Please enter a correct JSON string.");
+  }
+};
+
 submitBtn.addEventListener("click", () => {
   try {
     let csv = "";
     const parsedJSON = JSON.parse(jsonField.value);
+    console.log(typeof parsedJSON);
     const jsonKeys = Object.keys(parsedJSON);
     const jsonValues = Object.values(parsedJSON);
 
